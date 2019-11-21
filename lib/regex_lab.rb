e54cb81a214bc33a4dd88a0e374137f88a31a28e
William Pannell <lpann5@gmail.com>
2019-11-21 02:59:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,7 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-if word.match(/\b[aeiou]/)
-  return true
-else
-  return false
-end
+word.match(/\b[aeiou]/)
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
